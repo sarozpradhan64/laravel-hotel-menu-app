1ce5c58f2b965992c424891351246413f57bed68
saroz pradhan <sarozpradhan64@gmail.com>
2023-06-30 22:59:55 +0545

    Homepage installed

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.welcome');
 });
 
 Route::get('menu/{user_id}', [FrontendController::class, 'Menu'])->name('frontend.menu');
